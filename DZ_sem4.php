04564f2ad885ce840b37cb5a8872dc74e442e319
MazurovaNN <110212634+MazurovaNN@users.noreply.github.com>
2022-12-07 17:40:44 +0300

    DZ_sem4.php

---
+++ DZ_sem4.php
@@ -0,0 +1,8 @@
+<?php
+    $_profession = 'менеджер';
+    $_city_country = 'Санкт-Петербург, Россия';
+    $_email = 'anigama@mail.ru';
+    $_mobile = '+7 901 373 46 96';
+
+    echo $_profession
+?>
